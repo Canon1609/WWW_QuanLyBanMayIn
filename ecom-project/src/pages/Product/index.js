c65949500c0788a417913db98a416f454fcd6f60
nguyenphuochuyy <phuochuy10023@gmail.com>
2024-09-05 23:21:16 +0700

    [#8 Create product page]

---
+++ ecom-project/src/pages/Product/index.js
@@ -0,0 +1,44 @@
+import "./Product.scss"
+import { Button, Col, Row } from "antd";
+import ToolComponent from "../../components/ToolComponent";
+import TableProduct from "../../components/TableProduct";
+const Product = ()=> {
+    return (
+        <>
+            <div className="container">
+                <Row gutter={[20,20]}>
+                    <Col span={24} >
+                        <div className="product">
+                            <h3 className="product__title">Products</h3>
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row gutter={[20,20]}>
+                    <Col span={24}>
+                        <div className="manage_btn mt-10 pd-10">
+                            <Button className="btn-add" htmlType="button" type="primary">
+                                Add product
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+
+                <Row gutter={[20,20]}>
+                    <Col span={24} >
+                            <ToolComponent/>
+                    </Col>
+                </Row>
+
+                <Row gutter={[20,20]}>
+                    <Col span={24} >
+                            <TableProduct/>
+                    </Col>
+                </Row>
+
+
+            </div>
+        </>
+    )
+}
+export default Product;
